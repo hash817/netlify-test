3daf165ee9b121eb6e6cbfeddec3a921eef8e44a
Yiran <wangyiran817@gmail.com>
2025-05-12 02:00:46 +0800

    test

--- app/(tour pages)/destinations/[country]/components/tour-card-list.tsx
+++ app/(tour pages)/destinations/[country]/components/tour-card-list.tsx
@@ -5,52 +5,11 @@ import { FilterContext } from "../filter-context";
 import { TourCard } from "@/app/components/tour-card-v2";
 
 export function TourCardList({ tours }: { tours: Array<{}> }) {
-    const { activeFilters, setActiveFilters } = useContext<any>(FilterContext)
-    const [toursShown, setToursShown] = useState([]);
+    const [toursShown, setToursShown] = useState(tours);
 
-    useEffect(() => {
-        const filteredTours = {
-            all: tours.filter(tour => {
-                if (!activeFilters.timeLimited && !activeFilters.dayTour &&
-                    !activeFilters.multiDay && !activeFilters.transportation) {
-                    return true;
-                }
-                return (
-                    (activeFilters.timeLimited && tour.timeLimited) ||
-                    (activeFilters.dayTour && tour.dayTour) ||
-                    (activeFilters.multiDay && tour.multiDay) ||
-                    (activeFilters.transportation && tour.transportation)
-                );
-            }),
-            timeLimited: tours.filter(tour => tour.timeLimited),
-            dayTour: tours.filter(tour => tour.dayTour),
-            multiDay: tours.filter(tour => tour.multiDay),
-            transportation: tours.filter(tour => tour.transportation),
-        };
-        console.log(filteredTours['all'])
-        if (filteredTours['all'].length == 0) {
-            setToursShown(null);
-        } else {
-            setToursShown(filteredTours['all']);
-        }
 
-    }, [activeFilters, tours])
 
-    if (!toursShown && !Object.values(activeFilters).every(value => value === false)) {
-        return (
-            <div className="text-center py-10">
-                <p className="text-gray-500">No tours match your selected filters.</p>
-                <button
-                    className="cursor-pointer border-[0] mt-4! bg-(--primary-color) text-white px-6 py-2 rounded-full"
-                    onClick={() => setActiveFilters({ timeLimited: false, dayTour: false, multiDay: false, transportation: false })}
-                >
-                    Clear Filters
-                </button>
-            </div>
-        );
-    }
-
-    if (!toursShown && Object.values(activeFilters).every(value => value === false)) {
+    if (!toursShown) {
         return (
             <div className="text-center py-10">
                 <p className="text-gray-500">No tour packages found.</p>
--- app/(tour pages)/destinations/[country]/page.tsx
+++ app/(tour pages)/destinations/[country]/page.tsx
@@ -1,9 +1,5 @@
-import { FilterProvider } from "@/app/(tour pages)/destinations/[country]/filter-context";
-import { FilterTabs } from "@/app/components/filter-tabs";
 import { TourCardList } from "@/app/(tour pages)/destinations/[country]/components/tour-card-list";
-import SearchBar from '@/app/(tour pages)/destinations/[country]/components/search-bar';
-import { notFound } from 'next/navigation'
-import { isValidCountry } from "@/app/lib/valid-countries";
+
 
 function formatTitle(title) {
   if (title == 'all') return ''
@@ -17,34 +13,24 @@ function formatTitle(title) {
 export default async function CountryPage({ params }: { params: { country: string } }) {
   const { country } = await params
 
-  if (!isValidCountry(country)) {
-    notFound()
-  }
-
   const res = await fetch(
     `${process.env.NEXT_PUBLIC_ORIGIN_URL}/api/tour_packages/${encodeURIComponent(country)}`
   );
   const data = await res.json();
   return (
     <main className="mb-12!">
-      <FilterProvider>
+
         <section className="bg-light m-0 px-5 md:px-9 lg:px-30">
           <h1 className="text-kent-purple2 text-3xl md:text-4xl font-bold text-primary mb-8 relative inline-block">
             All {formatTitle(country)} Tour Packages
             <div className="mt-2 w-22 md:w-32 lg:w-36 h-1 bg-kent-red" />
           </h1>
-          <div className="bg-white shadow-md rounded-xl p-4">
-            <SearchBar
-              placeholder="Search for destinations, activities, or tour highlights..."
-              country={country}
-            />
-          </div>
-          <FilterTabs className="py-6 mb-10" />
+      
         </section>
         <section className="container">
           <TourCardList tours={data} />
         </section>
-      </FilterProvider>
+  
     </main>
   );
 }
