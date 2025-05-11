43f9ab78e026a3cad59f8c8db8ee3f00fe5c1d37
Yiran <wangyiran817@gmail.com>
2025-05-12 01:28:25 +0800

    test

--- app/destination/[country]/page.tsx
+++ app/destination/[country]/page.tsx
@@ -1,6 +1,14 @@
-import Link from "next/link";
+import { TourCardList } from "@/components/tour-card-list";
 
-export default async function Home({ params }: { params: { country: string } }) {
+function formatTitle(title) {
+  if (title == 'all') return ''
+  return title
+    .split('-')
+    .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(' ');
+}
+
+export default async function CountryPage({ params }: { params: { country: string } }) {
   const { country } = await params
 
   const res = await fetch(
@@ -8,10 +16,16 @@ export default async function Home({ params }: { params: { country: string } }) 
   );
   const data = await res.json();
   return (
-    <div className=" items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      {data.map(d => (
-        <div key={d.id}>{d.title}</div>
-      ))}
-    </div>
+    <main className="mb-12!">
+        <section className="m-0 px-5 md:px-9 lg:px-30">
+          <h1 className="text-black text-3xl md:text-4xl font-bold text-primary mb-8 relative inline-block">
+            All {formatTitle(country)} Tour Packages
+            <div className="mt-2 w-22 md:w-32 lg:w-36 h-1 bg-kent-red" />
+          </h1>
+        </section>
+        <section className="container">
+          <TourCardList tours={data} />
+        </section>
+    </main>
   );
 }
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [new URL('https://kentholidays.lck.com.sg/**')],
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
